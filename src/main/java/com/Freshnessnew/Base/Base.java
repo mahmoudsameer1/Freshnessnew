b27ee6cc6172ab53e338e83ed26a90b9c8470bd2
Mahmoud Alfarran <mahmoud.alfarran@pwc.com>
2024-12-09 14:17:46 +0300

    tesT

--- src/main/java/com/Freshnessnew/Base/Base.java
+++ src/main/java/com/Freshnessnew/Base/Base.java
@@ -103,8 +103,9 @@ public class Base {
             browserstackOptions.put("osVersion", osVersion);
             browserstackOptions.put("projectName", "Your Project Name");
             browserstackOptions.put("buildName", "Build Name");
-            browserstackOptions.put("local", "true");
-            browserstackOptions.put("uploadMedia", new JSONArray().put(BROWSERSTACK_MEDIA_URL));
+//            browserstackOptions.put("local", "false");
+            browserstackOptions.put("seleniumVersion", "4.27.0");
+//            browserstackOptions.put("uploadMedia", new JSONArray().put(BROWSERSTACK_MEDIA_URL));
             
             capabilities.setCapability("bstack:options", browserstackOptions);
             driver = new RemoteWebDriver(new URL(BROWSERSTACK_URL), capabilities);
